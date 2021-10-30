2de699df5d6391bf2194e765c12e07ed95010706
TheLastMagic <87115290+TheLastMagic@users.noreply.github.com>
2021-10-30 16:38:45 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "include/raylib.h"
+#include <stdlib.h>
 
 int main(void)
 {
@@ -6,19 +7,28 @@ int main(void)
     const int screenWidth = 800;
     const int screenHeight = 450;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    InitWindow(screenWidth, screenHeight, "Raylib Game");
 
-    SetTargetFPS(60);               
+    SetTargetFPS(60);
 
-    
+    int dotX = 300;
+    int dotY = 185;               
+
     while (!WindowShouldClose())    
     {
+
+        if (IsKeyDown(KEY_D)) dotX += 2.0f;
+        if (IsKeyDown(KEY_A)) dotX -= 2.0f;
+        if (IsKeyDown(KEY_W)) dotY -= 2.0f;
+        if (IsKeyDown(KEY_S)) dotY += 2.0f;
         
         BeginDrawing();
 
             ClearBackground(RAYWHITE);
 
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+
+            DrawCircle(dotX, dotY, 50, BLUE);
+            
 
         EndDrawing();
         
